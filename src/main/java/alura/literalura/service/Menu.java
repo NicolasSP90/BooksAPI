b9d5763aa36dfa7b7532e20f5851fcff177460ad
NicolasSP90 <nicolassp90@hotmail.com>
2025-01-14 21:57:25 -0300

    .\primeiro commit

---
+++ src/main/java/alura/literalura/service/Menu.java
@@ -0,0 +1,12 @@
+package alura.literalura.service;
+
+public class Menu {
+    public void showMenu() {
+        System.out.println("""
+                ===== Menu Inicial =====
+                
+                1- 
+                
+                """);
+    }
+}
